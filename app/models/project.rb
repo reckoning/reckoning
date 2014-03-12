819da985c5a31eb2b7a0b43a9696c90335be4dcb
Marten Klitzke <m.klitzke@gmail.com>
2014-03-13 00:44:12 +0100

    Add Uninvoiced Hours to Dashboard

--- app/models/project.rb
+++ app/models/project.rb
@@ -24,7 +24,35 @@ class Project < ActiveRecord::Base
     values
   end
 
+  def timer_values_invoiced
+    values = 0.0
+    timers.each do |timer|
+      if timer.value.present? && timer.position_id.present?
+        values += timer.value.to_d
+      end
+    end
+    values
+  end
+
+  def timer_values_uninvoiced
+    values = 0.0
+    timers.each do |timer|
+      if timer.value.present? && timer.position_id.blank?
+        values += timer.value.to_d
+      end
+    end
+    values
+  end
+
   def budget_percent
     timer_values / budget * 100
+  end
+
+  def budget_percent_invoiced
+    timer_values_invoiced / budget * 100
+  end
+
+  def budget_percent_uninvoiced
+    timer_values_uninvoiced / budget * 100
   end
 end
