188b120a53d40d089b2f472f03f074972052484b
Marten Klitzke <m.klitzke@gmail.com>
2014-04-28 23:13:08 +0200

    Fix abilities and Bump Version

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,10 +4,6 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
 
-    if user.admin?
-      can :manage, :all
-    end
-
     can :update, User, id: user.id
 
     can [:read, :create, :destroy, :check], Invoice, user_id: user.id
