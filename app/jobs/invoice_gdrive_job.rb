f660f6c986fa07712c53648ca9e28ebcee63cc74
Marten Klitzke <m.klitzke@gmail.com>
2014-04-29 00:08:02 +0200

    Update GDrive Folder Structure

--- app/jobs/invoice_gdrive_job.rb
+++ app/jobs/invoice_gdrive_job.rb
@@ -8,23 +8,15 @@ class InvoiceGdriveJob
     if invoice.present?
       session = GoogleDrive.login(invoice.user.gdrive_email, invoice.user.gdrive_password)
 
-      collection = session.collection_by_title(invoice.user.gdrive_collection)
-      if collection.blank?
-        collection = session.root_collection.create_subcollection(invoice.user.gdrive_collection)
-      end
+      collection = build_collection_path(invoice, session)
 
-      customer_collection = collection.subcollection_by_title(invoice.customer.fullname)
-      if customer_collection.blank?
-        customer_collection = collection.create_subcollection(invoice.customer.fullname)
-      end
-
-      invoice_file = customer_collection.files(title: invoice.invoice_file)
+      invoice_file = collection.files(title: invoice.invoice_file)
       if invoice_file.present?
         invoice_file.first.delete(true)
       end
       invoice_file = session.upload_from_file(invoice.pdf_path, invoice.invoice_file, convert: false)
 
-      customer_collection.add invoice_file
+      collection.add invoice_file
       session.root_collection.remove invoice_file
 
       if File.exists?(invoice.timesheet_path)
@@ -35,9 +27,39 @@ class InvoiceGdriveJob
 
         timesheet_file = session.upload_from_file(invoice.timesheet_path, invoice.timesheet_file, convert: false)
 
-        customer_collection.add timesheet_file
+        collection.add timesheet_file
         session.root_collection.remove timesheet_file
       end
     end
+  end
+
+  protected
+
+  def self.build_collection_path invoice, session
+    default_collection = Settings.app.gdrive_collection
+    default_collection = invoice.user.gdrive_collection unless invoice.user.gdrive_collection.blank?
+
+    start_collection = session.collection_by_title(default_collection)
+
+    if start_collection.blank?
+      start_collection = session.root_collection.create_subcollection(default_collection)
+    end
+
+    year_collection = start_collection.subcollection_by_title(invoice.date.year)
+    if year_collection.blank?
+      year_collection = start_collection.create_subcollection(invoice.date.year)
+    end
+
+    customer_collection = year_collection.subcollection_by_title(invoice.customer.fullname)
+    if customer_collection.blank?
+      customer_collection = year_collection.create_subcollection(invoice.customer.fullname)
+    end
+
+    project_collection = customer_collection.subcollection_by_title(invoice.project.name)
+    if project_collection.blank?
+      project_collection = customer_collection.create_subcollection(invoice.project.name)
+    end
+
+    return project_collection
   end
 end
