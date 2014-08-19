b0e92b710c02c45c6d19a7a308d3cdee2894a343
Marten Klitzke <m.klitzke@gmail.com>
2014-08-19 23:16:23 +0200

    fix nested form for week and timers to reject empty timers

--- app/models/week.rb
+++ app/models/week.rb
@@ -4,17 +4,12 @@ class Week < ActiveRecord::Base
   has_and_belongs_to_many :tasks
   has_many :projects, through: :tasks
 
-  accepts_nested_attributes_for :timers, allow_destroy: true
+  accepts_nested_attributes_for :timers, allow_destroy: true, reject_if: :reject_timers
 
-
-  before_save :reject_empty_timers
-
-
-  def reject_empty_timers
-    timers.reject do |timer|
-      if timer.value.blank?
-        timers.delete(timer)
-      end
-    end
+  def reject_timers attributes
+    exists = attributes['id'].present?
+    empty = attributes[:value].blank?
+    attributes.merge!({:_destroy => 1}) if exists and empty
+    return (!exists and empty)
   end
 end
