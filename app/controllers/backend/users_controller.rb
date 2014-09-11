ac3e906db4ca4440246e6c0be9577df4ac3a6b18
Marten Klitzke <m.klitzke@gmail.com>
2014-09-11 22:14:09 +0300

    update backend users #bugfix

--- app/controllers/backend/users_controller.rb
+++ app/controllers/backend/users_controller.rb
@@ -1,28 +1,39 @@
 module Backend
   class UsersController < BaseController
-    before_action :set_user, only: [:show, :edit, :update, :destroy]
     before_action :set_active_nav
 
     # get: /backend/users
     def index
       @users = User.all
-        .order(sort_column + " " + sort_direction)
+        .order("#{sort_column} #{sort_direction}")
         .page(params.fetch(:page){nil})
         .per(20)
     end
 
     # get: /backend/users/new
     def new
-      @user = User.new
     end
 
     # post: /backend/users
     def create
-      @user = User.new(user_params)
-      if @user.save
+      password = Devise.friendly_token.first(30)
+      @user = User.new(user_params.merge(
+        password: password,
+        password_confirmation: password
+      ))
+      user.skip_confirmation_notification!
+      if user.save
         redirect_to backend_users_path, notice: I18n.t(:"messages.user.create.success")
       else
         render 'new', error: I18n.t(:"messages.user.create.failure")
+      end
+    end
+
+    def send_welcome
+      if user.send_confirmation_instructions
+        redirect_to backend_users_path, notice: I18n.t(:"messages.user.send_welcome.success")
+      else
+        redirect_to backend_users_path, notice: I18n.t(:"messages.user.send_welcome.failure")
       end
     end
 
@@ -32,7 +43,7 @@ module Backend
 
     # patch: /backend/users/:id
     def update
-      if @user.update(user_params)
+      if user.update(user_params)
         redirect_to backend_users_path, notice: I18n.t(:"messages.user.create.success")
       else
         render "edit", error: I18n.t(:"messages.user.update.failure")
@@ -40,7 +51,7 @@ module Backend
     end
 
     def destroy
-      if @user.destroy
+      if user.destroy
         redirect_to backend_users_path, notice: I18n.t(:"messages.user.destroy.success")
       else
         redirect_to backend_users_path, error: I18n.t(:"messages.user.destroy.failure")
@@ -55,12 +66,14 @@ module Backend
     private
 
     def user_params
-      params.require(:user).permit(:email, :admin, :enabled)
+      params.require(:user).permit(:email, :password, :password_confirmation, :admin, :enabled)
     end
 
-    def set_user
-      @user = User.find(params[:id])
+    def user
+      @user ||= User.where(id: params.fetch(:id, nil)).first
+      @user ||= User.new
     end
+    helper_method :user
 
     def set_active_nav
       @active_nav = 'backend_users'
