4a2c9046b6fa5b60e20f7d9341093c1f535d204d
Marten Klitzke <m.klitzke@gmail.com>
2014-09-11 22:14:31 +0300

    some random improvements

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,6 @@ class Task < ActiveRecord::Base
   validates_presence_of :project_id, :name
 
   belongs_to :project
-  belongs_to :user
   has_many :timers, dependent: :destroy
   has_and_belongs_to_many :weeks
 
