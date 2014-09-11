95b6c3c4263b4891677b86cb5c478b208657240b
Marten Klitzke <m.klitzke@gmail.com>
2014-09-11 22:08:23 +0300

    switch back to fixtures for testing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,34 +4,10 @@ require File.expand_path('../../config/environment', __FILE__)
 require "rails/test_help"
 require "minitest/rails"
 
-require "database_cleaner"
-
-# database cleaner
-DatabaseCleaner.strategy = :transaction
-
-# factory girl
-require "factory_girl"
-FactoryGirl.find_definitions
-
-class Minitest::Spec
-  include FactoryGirl::Syntax::Methods
-end
-
-
-class ActiveSupport::TestCase
-  include FactoryGirl::Syntax::Methods
-end
+require "active_record/fixtures"
 
 class ActionController::TestCase
   include FactoryGirl::Syntax::Methods
   include Devise::TestHelpers
   ActiveRecord::Migration.check_pending!
-
-  before do
-    DatabaseCleaner.start
-  end
-
-  after do
-    DatabaseCleaner.clean
-  end
 end
