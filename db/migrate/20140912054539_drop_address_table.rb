5d9a56b19647c8859242f3d09e4c7232cff05842
Marten Klitzke <m.klitzke@gmail.com>
2014-09-12 10:48:31 +0300

    squash migrations

--- db/migrate/20140912054539_drop_address_table.rb
+++
@@ -1,9 +0,0 @@
-class DropAddressTable < ActiveRecord::Migration
-  def up
-    drop_table :addresses
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration, "Wont go back to plain ids"
-  end
-end
