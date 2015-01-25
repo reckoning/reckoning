3de2c38dcd4a409b25c486098c95b8b3104aad4e
Marten Klitzke <m.klitzke@gmail.com>
2015-01-25 19:43:24 +0100

    change email validation

--- app/models/test_mail.rb
+++ app/models/test_mail.rb
@@ -10,5 +10,5 @@ class TestMail < ActiveRecord::Base
   column :email, :string
 
   validates_presence_of :email
-  validates_with EmailValidator
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
