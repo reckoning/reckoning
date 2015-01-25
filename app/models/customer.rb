8a8ce48202f3de5ff05adeb3c9ec653498b6b304
Marten Klitzke <m.klitzke@gmail.com>
2015-01-25 19:43:24 +0100

    change email validation

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -14,6 +14,8 @@ class Customer < ActiveRecord::Base
     end
   end
 
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+
   def fullname
     company_and_name = []
     company_and_name << name if name.present?
