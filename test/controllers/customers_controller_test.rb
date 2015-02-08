a7e2d4729ff99025a6084c75e4c27b5b5b95b897
Marten Klitzke <m.klitzke@gmail.com>
2015-02-08 18:51:55 +0100

    fix tests for projects

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -16,15 +16,6 @@ class CustomersControllerTest < ActionController::TestCase
       assert_equal I18n.t(:"devise.failure.unauthenticated"), flash[:alert]
     end
 
-    it "Unauthrized user cant destroy customer" do
-      delete :destroy, id: customer.id
-
-      assert_response :found
-      assert_equal I18n.t(:"devise.failure.unauthenticated"), flash[:alert]
-
-      assert_equal customer, Customer.where(id: customer.id).first
-    end
-
     it "Unauthrized user cant update customer" do
       put :update, id: customer.id, customer: { name: "bar" }
 
@@ -49,17 +40,6 @@ class CustomersControllerTest < ActionController::TestCase
 
       assert_response :found
       assert_equal I18n.t(:"messages.customer.update.success"), flash[:notice]
-    end
-
-    it "User can destroy customer" do
-      klingon = customers :klingon
-      delete :destroy, id: klingon.id
-
-      assert_response :found
-
-      assert_equal I18n.t(:"messages.customer.destroy.success"), flash[:notice]
-
-      assert_not_equal klingon, Customer.where(id: klingon.id).first
     end
   end
 end
