7838f7c41fde14f21cca3b140d0a45ea3bec5a61
Marten Klitzke <m.klitzke@gmail.com>
2015-02-09 20:51:14 +0100

    add responsive button groups

--- config/application.rb
+++ config/application.rb
@@ -31,12 +31,6 @@ module Reckoning
     config.middleware.insert_before 0, "Rack::Cors", debug: true, logger: (-> { Rails.logger }) do
       allow do
         origins '*'
-
-        resource '/cors', headers: :any,
-                          methods: [:post],
-                          credentials: true,
-                          max_age: 0
-
         resource '*', headers: :any,
                       methods: [:get, :post, :delete, :put, :options, :head],
                       max_age: 0
