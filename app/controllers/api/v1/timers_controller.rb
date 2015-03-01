c269b9237d69a1b5c6f392356f19a7035f5658f3
Marten Klitzke <m.klitzke@gmail.com>
2015-03-01 19:02:30 +0100

    only allow start and stop actions and valid timer entries

--- app/controllers/api/v1/timers_controller.rb
+++ app/controllers/api/v1/timers_controller.rb
@@ -30,20 +30,27 @@ module Api
 
       def stop
         authorize! :stop, timer
-        if timer.update(started: false, value: timer.value + ((Time.now - timer.started_at) / 1.hour))
-          render json: timer, status: :ok
+        if timer.started
+          if timer.update(started: false, value: timer.value + ((Time.now - timer.started_at) / 1.hour))
+            render json: timer, status: :ok
+          else
+            render json: timer.errors, status: :bad_request
+          end
         else
-          render json: timer.errors, status: :bad_request
-        end
+          render json: { message: I18n.t(:"messages.timer.stop.failure") }, status: :bad_request
+        else
       end
 
       def start
         authorize! :start, timer
-        if timer.update(started: true)
-          render json: timer, status: :ok
+        if timer.started
+          render json: { message: I18n.t(:"messages.timer.start.failure") }, status: :bad_request
         else
-          render json: timer.errors, status: :bad_request
-        end
+          if timer.update(started: true)
+            render json: timer, status: :ok
+          else
+            render json: timer.errors, status: :bad_request
+          end
       end
 
       def destroy
