daecceaa43cbb3730c7d815ffe2d583ada370db2
Marten Klitzke <mklitzke@mortik.de>
2015-06-05 20:08:17 +0200

    Fix tests

--- db/schema.rb
+++ db/schema.rb
@@ -37,29 +37,29 @@ ActiveRecord::Schema.define(version: 20150301161658) do
     t.integer  "address_id"
     t.integer  "payment_due"
     t.text     "email_template"
-    t.string   "invoice_email",       limit: 255
-    t.string   "default_from",        limit: 255
+    t.string   "invoice_email"
+    t.string   "default_from"
     t.hstore   "contact_information"
-    t.string   "name",                limit: 255
-    t.uuid     "account_id",                      null: false
+    t.string   "name"
+    t.uuid     "account_id",          null: false
   end
 
   create_table "invoices", id: :uuid, default: "uuid_generate_v4()", force: :cascade do |t|
     t.date     "date"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.decimal  "value",                        precision: 10, scale: 2, default: 0.0,   null: false
-    t.decimal  "rate",                         precision: 10, scale: 2, default: 0.0,   null: false
-    t.string   "state",            limit: 255
+    t.decimal  "value",            precision: 10, scale: 2, default: 0.0,   null: false
+    t.decimal  "rate",             precision: 10, scale: 2, default: 0.0,   null: false
+    t.string   "state"
     t.datetime "pay_date"
     t.integer  "ref"
-    t.boolean  "pdf_generating",                                        default: false, null: false
+    t.boolean  "pdf_generating",                            default: false, null: false
     t.date     "delivery_date"
     t.date     "payment_due_date"
     t.datetime "pdf_generated_at"
     t.uuid     "customer_id"
     t.uuid     "project_id"
-    t.uuid     "account_id",                                                            null: false
+    t.uuid     "account_id",                                                null: false
   end
 
   create_table "positions", id: :uuid, default: "uuid_generate_v4()", force: :cascade do |t|
@@ -73,18 +73,18 @@ ActiveRecord::Schema.define(version: 20150301161658) do
   end
 
   create_table "projects", id: :uuid, default: "uuid_generate_v4()", force: :cascade do |t|
-    t.string   "name",                limit: 255
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.decimal  "rate",                            precision: 10, scale: 2, default: 0.0,      null: false
-    t.decimal  "budget",                          precision: 10, scale: 2, default: 0.0,      null: false
+    t.decimal  "rate",                precision: 10, scale: 2, default: 0.0,      null: false
+    t.decimal  "budget",              precision: 10, scale: 2, default: 0.0,      null: false
     t.uuid     "customer_id"
-    t.boolean  "budget_on_dashboard",                                      default: true
-    t.string   "state",                                                    default: "active", null: false
+    t.boolean  "budget_on_dashboard",                          default: true
+    t.string   "state",                                        default: "active", null: false
   end
 
   create_table "tasks", id: :uuid, default: "uuid_generate_v4()", force: :cascade do |t|
-    t.string   "name",       limit: 255
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.uuid     "project_id"
@@ -104,41 +104,41 @@ ActiveRecord::Schema.define(version: 20150301161658) do
   end
 
   create_table "users", id: :uuid, default: "uuid_generate_v4()", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "",    null: false
-    t.string   "encrypted_password",     limit: 255, default: "",    null: false
-    t.string   "reset_password_token",   limit: 255
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
-    t.integer  "failed_attempts",                    default: 0
-    t.string   "unlock_token",           limit: 255
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.integer  "failed_attempts",        default: 0
+    t.string   "unlock_token"
     t.datetime "locked_at"
-    t.string   "authentication_token",   limit: 255
+    t.string   "authentication_token"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "address_id"
-    t.boolean  "enabled",                            default: false
-    t.boolean  "admin",                              default: false
-    t.string   "confirmation_token",     limit: 255
+    t.boolean  "enabled",                default: false
+    t.boolean  "admin",                  default: false
+    t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email",      limit: 255
-    t.string   "gravatar",               limit: 255
-    t.string   "gravatar_hash",          limit: 255
-    t.string   "plan",                   limit: 255
-    t.uuid     "account_id",                                         null: false
+    t.string   "unconfirmed_email"
+    t.string   "gravatar"
+    t.string   "gravatar_hash"
+    t.string   "plan"
+    t.uuid     "account_id",                             null: false
     t.string   "name"
     t.string   "totp_auth_secret"
     t.string   "totp_recovery_secret"
-    t.boolean  "totp_enabled",                       default: false, null: false
-    t.boolean  "totp_mandatory",                     default: false, null: false
+    t.boolean  "totp_enabled",           default: false, null: false
+    t.boolean  "totp_mandatory",         default: false, null: false
     t.datetime "totp_enabled_on"
-    t.integer  "totp_failed_attempts",               default: 0,     null: false
-    t.integer  "totp_recovery_counter",              default: 0,     null: false
+    t.integer  "totp_failed_attempts",   default: 0,     null: false
+    t.integer  "totp_recovery_counter",  default: 0,     null: false
     t.string   "totp_persistence_seed"
     t.string   "totp_session_challenge"
     t.datetime "totp_challenge_expires"
