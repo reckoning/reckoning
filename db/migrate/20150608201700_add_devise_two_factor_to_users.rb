5987776ee8f276dcac7e6de2874ae77938cfb892
Marten Klitzke <mklitzke@mortik.de>
2015-06-09 02:49:00 +0200

    init user with otp secret on create

--- db/migrate/20150608201700_add_devise_two_factor_to_users.rb
+++ db/migrate/20150608201700_add_devise_two_factor_to_users.rb
@@ -4,5 +4,10 @@ class AddDeviseTwoFactorToUsers < ActiveRecord::Migration
     add_column :users, :encrypted_otp_secret_iv, :string
     add_column :users, :encrypted_otp_secret_salt, :string
     add_column :users, :otp_required_for_login, :boolean
+
+    User.all.each do |user|
+      user.otp_secret = User.generate_otp_secret
+      user.save!
+    end
   end
 end
