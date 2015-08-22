c24853a3be25f4ef02367a8792391840197ccd06
Marten Klitzke <mklitzke@mortik.de>
2015-08-23 00:10:56 +0200

    update validators

--- app/validators/email_validator.rb
+++ app/validators/email_validator.rb
@@ -1,6 +1,18 @@
 class EmailValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    return if value.blank? || value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+    return if nil?(value) || blank?(value) || valid?(value)
     record.errors[attribute] << (options[:message] || "not a valid email")
+  end
+
+  private def blank?(value)
+    value.blank? && options[:allow_blank]
+  end
+
+  private def nil?(value)
+    value.nil? && options[:allow_nil]
+  end
+
+  private def valid?(value)
+    value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   end
 end
