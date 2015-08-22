ac32aa7c7937817a6e97b38b2498a41529a014dd
Marten Klitzke <mklitzke@mortik.de>
2015-08-23 00:10:56 +0200

    update validators

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,5 +6,5 @@ class Customer < ActiveRecord::Base
   store_accessor :contact_information, :address, :country, :email, :telefon, :fax, :website
 
   validates :name, presence: true
-  validates :email, email: true
+  validates :email, email: true, allow_blank: true
 end
--- app/validators/uuid_validator.rb
+++ app/validators/uuid_validator.rb
@@ -1,6 +1,18 @@
 class UuidValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    return if value.nil? || value =~ /\A[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}\z/i
+    return if nil?(value) || blank?(value) || valid?(value)
     record.errors[attribute] << (options[:message] || "not a valid uuid")
+  end
+
+  private def blank?(value)
+    value.blank? && options[:allow_blank]
+  end
+
+  private def nil?(value)
+    value.nil? && options[:allow_nil]
+  end
+
+  private def valid?(value)
+    value =~ /\A[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}\z/i
   end
 end
