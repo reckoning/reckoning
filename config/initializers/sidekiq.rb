8769e5b3a610fd63b219df69dd027f8eab162b4c
Marten Klitzke <me@mortik.xyz>
2015-12-31 15:53:35 +0100

    update sidekiq initializer

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { size: 4 }
+  config.redis = { size: ENV["SIDEKIQ_SERVER_SIZE"] || 1 }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { size: 6 }
+  config.redis = { size: ENV["SIDEKIQ_CLIENT_SIZE"] || 1 }
 end
