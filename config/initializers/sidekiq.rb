f9563eedb9f7b7a60f52503b5df0d94c2cb6b19d
Marten Klitzke <me@mortik.xyz>
2015-12-31 16:14:08 +0100

    try to fix sidekiq connections

--- config/initializers/sidekiq.rb
+++
@@ -1,7 +0,0 @@
-Sidekiq.configure_server do |config|
-  config.redis = { size: ENV["SIDEKIQ_SERVER_SIZE"] || 1 }
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = { size: ENV["SIDEKIQ_CLIENT_SIZE"] || 1 }
-end
