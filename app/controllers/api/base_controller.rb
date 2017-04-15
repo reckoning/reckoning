8e0682776ba87a8d68602cb9bdb422fde7840c4c
Marten Klitzke <me@mortik.xyz>
2017-04-15 14:03:06 +0200

    chore(jwt): add missing require

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,5 +1,7 @@
 # encoding: utf-8
 # frozen_string_literal: true
+require 'json_web_token'
+
 module Api
   class BaseController < ActionController::Base
     include ActionController::HttpAuthentication::Token
