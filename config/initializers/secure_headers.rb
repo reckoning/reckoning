15a3116cbd976bfcf2a0090d338896b830b8c892
Marten Klitzke <me@mortik.xyz>
2017-06-14 10:34:01 +0200

    Update refererer

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -16,7 +16,7 @@ SecureHeaders::Configuration.default do |config|
   config.x_xss_protection = '1; mode=block'
   config.x_download_options = 'noopen'
   config.x_permitted_cross_domain_policies = 'none'
-  config.referrer_policy = 'origin-when-cross-origin'
+  config.referrer_policy = 'no-referrer'
   config.clear_site_data = %w[
     cache
     cookies
