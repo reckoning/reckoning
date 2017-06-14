0f2df5a0b20b87d087852120439b6c693bc039c4
Marten Klitzke <me@mortik.xyz>
2017-06-14 10:05:28 +0200

    Update secure headers

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -12,11 +12,11 @@ SecureHeaders::Configuration.default do |config|
   # Add "; preload" and submit the site to hstspreload.org for best protection.
   config.hsts = nil
   config.x_frame_options = nil
-  config.x_content_type_options = "nosniff"
-  config.x_xss_protection = "1; mode=block"
-  config.x_download_options = "noopen"
-  config.x_permitted_cross_domain_policies = "none"
-  config.referrer_policy = "strict-origin-when-cross-origin"
+  config.x_content_type_options = 'nosniff'
+  config.x_xss_protection = '1; mode=block'
+  config.x_download_options = 'noopen'
+  config.x_permitted_cross_domain_policies = 'none'
+  config.referrer_policy = 'origin-when-cross-origin'
   config.clear_site_data = %w[
     cache
     cookies
@@ -24,20 +24,33 @@ SecureHeaders::Configuration.default do |config|
     executionContexts
   ]
 
+  # rubocop:disable Lint/PercentStringArray
   config.csp = {
-    preserve_schemes: true, # default: false. Schemes are removed from host sources to save bytes and discourage mixed content.
+    preserve_schemes: true,
 
-    default_src: %w[https: self],
-    base_uri: %w[self],
+    default_src: %w[https: 'self'],
+    base_uri: %w['self'],
     block_all_mixed_content: true,
-    child_src: %w[self],
+    child_src: %w['self'],
     connect_src: %w[wss:],
-    font_src: %w[self data:],
-    frame_ancestors: %w[none],
-    object_src: %w[self],
-    script_src: %w[self],
-    style_src: %w[unsafe-inline],
+    font_src: %w['self' data:],
+    frame_ancestors: %w['none'],
+    object_src: %w['self'],
+    script_src: %w['self', 'unsafe-inline'],
+    style_src: %w['self', 'unsafe-inline', 'fonts.googleapis.com'],
     upgrade_insecure_requests: true,
-    report_uri: %w[https://053645df64da2ba3e3f2b3b53e6e95b0.report-uri.io/r/default/csp/enforce]
+    report_uri: %w[https://mortik.report-uri.io/r/default/csp/enforce]
+  }
+
+  config.hpkp = {
+    report_only: false,
+    max_age: 60.days.to_i,
+    include_subdomains: true,
+    report_uri: "https://mortik.report-uri.io/r/default/hpkp/enforce",
+    pins: [
+      { sha256: "QDswWuU7+p+bL6X8qrjr9lj3qpkz8veaoqsaDa3Ys+I=" },
+      { sha256: "YLh1dUR9y6Kja30RrAn7JKnbQG/uEtLMkBgFF2Fuihg=" },
+      { sha256: "Vjs8r4z+80wjNcr1YKepWQboSIRi63WsWXhIMN+eWys=" }
+    ]
   }
 end
