971f62f6f1c6e22d0727f30dc36ea8c710b49071
Marten Klitzke <me@mortik.xyz>
2017-07-16 15:20:57 +0200

    Add home office to expense types

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -4,7 +4,7 @@
 class Expense < ApplicationRecord
   belongs_to :account
 
-  VALID_TYPES = %i[gwg afa licenses telecommunication current misc].freeze
+  VALID_TYPES = %i[gwg afa licenses telecommunication home_office current misc].freeze
   NEEDS_RECEIPT_TYPES = VALID_TYPES.reject { |type| %i[telecommunication current].include?(type) }.freeze
 
   attachment :receipt, content_type: ["application/pdf", "image/jpeg", "image/png"]
