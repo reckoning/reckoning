b3575e31789f47dbccb9a77a5d9a31ccfa89c15f
Marten Klitzke <me@mortik.xyz>
2017-07-17 23:13:47 +0200

    Sort Expenses in PDF by Date

--- app/models/expense_pdf.rb
+++ app/models/expense_pdf.rb
@@ -15,17 +15,17 @@ class ExpensePdf
     self.account = account
     self.filter = filter
 
-    self.telecommunication = expenses.select { |expense| expense.expense_type == 'telecommunication' }
-    self.home_office = expenses.select { |expense| expense.expense_type == 'home_office' }
-    self.gwg = expenses.select { |expense| expense.expense_type == 'gwg' }
-    self.misc = expenses.select { |expense| expense.expense_type == 'misc' }
-    self.current = expenses.select { |expense| expense.expense_type == 'current' }
-    self.licenses = expenses.select { |expense| expense.expense_type == 'licenses' }
-    self.travel_costs = expenses.select { |expense| expense.expense_type == 'travel_costs' }
-    self.business_expenses = expenses.select { |expense| expense.expense_type == 'business_expenses' }
-    self.training = expenses.select { |expense| expense.expense_type == 'training' }
-    self.non_cash_contribution = expenses.select { |expense| expense.expense_type == 'non_cash_contribution' }
-    self.insurances = expenses.select { |expense| expense.expense_type == 'insurances' }
+    self.telecommunication = expenses.select { |expense| expense.expense_type == 'telecommunication' }.sort_by(&:date)
+    self.home_office = expenses.select { |expense| expense.expense_type == 'home_office' }.sort_by(&:date)
+    self.gwg = expenses.select { |expense| expense.expense_type == 'gwg' }.sort_by(&:date)
+    self.misc = expenses.select { |expense| expense.expense_type == 'misc' }.sort_by(&:date)
+    self.current = expenses.select { |expense| expense.expense_type == 'current' }.sort_by(&:date)
+    self.licenses = expenses.select { |expense| expense.expense_type == 'licenses' }.sort_by(&:date)
+    self.travel_costs = expenses.select { |expense| expense.expense_type == 'travel_costs' }.sort_by(&:date)
+    self.business_expenses = expenses.select { |expense| expense.expense_type == 'business_expenses' }.sort_by(&:date)
+    self.training = expenses.select { |expense| expense.expense_type == 'training' }.sort_by(&:date)
+    self.non_cash_contribution = expenses.select { |expense| expense.expense_type == 'non_cash_contribution' }.sort_by(&:date)
+    self.insurances = expenses.select { |expense| expense.expense_type == 'insurances' }.sort_by(&:date)
   end
 
   def persisted?
