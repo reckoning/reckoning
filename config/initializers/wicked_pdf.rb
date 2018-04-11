7fe72d97c454e30d2715d4b0f49150d5729fdba4
Marten Klitzke <me@mortik.xyz>
2018-04-11 19:32:20 +0200

    chore(ci): update gitlab conf

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,13 +1,5 @@
 # frozen_string_literal: true
 
 if Rails.env.development? || Rails.env.test?
-  WickedPdf.config = if ENV["TRAVIS_CI"]
-                       {
-                         exe_path: "/usr/bin/wkhtmltopdf"
-                       }
-                     else
-                       {
-                         exe_path: `which wkhtmltopdf`.strip
-                       }
-                     end
+  WickedPdf.config = { exe_path: `which wkhtmltopdf`.strip }
 end
