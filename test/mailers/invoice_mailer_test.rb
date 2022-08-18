0ce1917657996219952afb239e97d88d5a9d8dba
Marten Klitzke <marten.klitzke@hey.com>
2022-08-18 21:15:07 +0200

    chore: update mailer default from and cable url

--- test/mailers/invoice_mailer_test.rb
+++ test/mailers/invoice_mailer_test.rb
@@ -7,7 +7,7 @@ class InvoiceMailerTest < ActionMailer::TestCase
 
   before do
     invoice.customer.email_template = 'Hallo Foo'
-    invoice.customer.invoice_email = 'test@customer.io'
+    invoice.customer.invoice_email = 'test@customer.cc'
     invoice.customer.save
   end
 
@@ -17,32 +17,32 @@ class InvoiceMailerTest < ActionMailer::TestCase
 
       assert_not ActionMailer::Base.deliveries.empty?
 
-      assert_equal ['test@customer.io'], mail.to
-      assert_equal ['noreply@mortik.xyz'], mail.from
+      assert_equal ['test@customer.cc'], mail.to
+      assert_equal ['noreply@reckoning.cc'], mail.from
     end
 
     it 'sends email to global from address' do
-      invoice.account.default_from = 'user@reckoning.io'
+      invoice.account.default_from = 'user@reckoning.cc'
       invoice.account.save
 
       mail = InvoiceMailer.customer(invoice).deliver_now
 
       assert_not ActionMailer::Base.deliveries.empty?
 
-      assert_equal ['test@customer.io'], mail.to
-      assert_equal ['user@reckoning.io'], mail.from
+      assert_equal ['test@customer.cc'], mail.to
+      assert_equal ['user@reckoning.cc'], mail.from
     end
 
     it 'sends email to customer from address' do
-      invoice.customer.default_from = 'special-customer@reckoning.io'
+      invoice.customer.default_from = 'special-customer@reckoning.cc'
       invoice.customer.save
 
       mail = InvoiceMailer.customer(invoice).deliver_now
 
       assert_not ActionMailer::Base.deliveries.empty?
 
-      assert_equal ['test@customer.io'], mail.to
-      assert_equal ['special-customer@reckoning.io'], mail.from
+      assert_equal ['test@customer.cc'], mail.to
+      assert_equal ['special-customer@reckoning.cc'], mail.from
     end
 
     it 'falls back to default from address if global email is empty string' do
@@ -53,8 +53,8 @@ class InvoiceMailerTest < ActionMailer::TestCase
 
       assert_not ActionMailer::Base.deliveries.empty?
 
-      assert_equal ['test@customer.io'], mail.to
-      assert_equal ['noreply@mortik.xyz'], mail.from
+      assert_equal ['test@customer.cc'], mail.to
+      assert_equal ['noreply@reckoning.cc'], mail.from
     end
   end
 end
