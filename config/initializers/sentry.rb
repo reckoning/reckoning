86f6b898889c97c8ee26ab0bb3c204d5b266e965
Marten Klitzke <marten.klitzke@hey.com>
2022-09-06 21:48:01 +0200

    chore(sentry): fix tags

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -3,8 +3,7 @@
 Sentry.init do |config|
   config.dsn = Rails.application.credentials.sentry_dsn
   config.release = Git.revision_short
-  config.tags = {
-    version: Reckoning::VERSION,
-    codename: Reckoning::CODENAME
-  }
 end
+
+Sentry.set_tags(version: Reckoning::VERSION)
+Sentry.set_tags(codename: Reckoning::CODENAME)
