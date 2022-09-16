480df55a6e0420dfba3e665ce71aebf44d1769a6
Marten Klitzke <marten.klitzke@hey.com>
2022-09-16 16:37:20 +0200

    chore: fix schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -41,6 +41,18 @@ ActiveRecord::Schema.define(version: 2022_09_16_142926) do
     t.text "offer_headline"
   end
 
+  create_table "auth_tokens", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
+    t.uuid "user_id", null: false
+    t.string "token"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "user_agent"
+    t.integer "expires"
+    t.index ["token", "user_id"], name: "index_auth_tokens_on_token_and_user_id", unique: true
+    t.index ["token"], name: "index_auth_tokens_on_token"
+  end
+
   create_table "customers", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
