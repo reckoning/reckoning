06b08c9e76abee53c338202e158f16a6ae7510d1
Marten Klitzke <marten.klitzke@hey.com>
2023-04-07 11:36:28 +0200

    chore: update initializer

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,3 +1,5 @@
 # frozen_string_literal: true
 
-WickedPdf.config = {exe_path: `which wkhtmltopdf`.strip} if Rails.env.development? || Rails.env.test?
+WickedPdf.config = {
+  exe_path: `which wkhtmltopdf`.strip
+} if Rails.env.development? || Rails.env.test?
